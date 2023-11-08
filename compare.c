d976fe640c1ac6f800bb471d2f9514119fbc5b73
Lalo <114935993+lalolel@users.noreply.github.com>
2023-11-08 17:40:36 +0200

    Update compare.c

--- compare.c
+++ compare.c
@@ -8,13 +8,13 @@ int main(void)
 
 if (x < y)
 {
-printf("x is less than y/n");
+printf("x is less than y\n");
 }
 else if (x > y)
 {
-printf("x is greater than y/n");
+printf("x is greater than y\n");
 }
 else
 {
-printf("x is equal to y/n");
+printf("x is equal to y\n");
 }
