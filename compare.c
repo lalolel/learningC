18e0e613f26e8cd361b4b1903e2a2494629ffe2b
Lalo <114935993+lalolel@users.noreply.github.com>
2023-11-08 17:26:41 +0200

    Create compare.c
    conditionals in C

---
+++ compare.c
@@ -0,0 +1,20 @@
+#include <cs50.h>   // for get int function
+#include <stdio.h>      // for printf function
+
+int main(void)
+{
+  int x = get_int("What's x? ");
+  int y = get_int("What's y? ");
+
+if (x < y)
+{
+printf("x is less than y/n");
+}
+else if (x > y)
+{
+printf("x is greater than y/n");
+}
+else
+{
+printf("x is equal to y/n");
+}
